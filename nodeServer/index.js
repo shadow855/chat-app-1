854c917875cc169ff3f5580693009f96f198424f
shadow855 <82103149+shadow855@users.noreply.github.com>
2024-05-27 16:11:45 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,20 +1,20 @@
-//Noder server which will handle socket io connections
-// const io = require('socket.io')(8000)
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-const path = require('path'); // Import path module 
+const path = require('path'); // Import path module
 
 const app = express();
 app.use(cors());
 
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '..')));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://127.0.0.1:5500",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
@@ -22,28 +22,25 @@ const io = new Server(server, {
 const users = {};
 
 io.on('connection', socket => {
-    //If any user joins, let other users connected to the server know!
     socket.on('new-user-joined', name => {
-        // console.log("New User: ", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
-    //If someone sends a message, broadcast it to other people
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
-    //If someone leaves the chat, let others know
-    socket.on('disconnect', message => {
+    socket.on('disconnect', () => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 });
 
-// Serve static files from the root directory
-const publicDirectoryPath = path.join(__dirname);
-app.use(express.static(publicDirectoryPath));
+// Serve index.html on the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 server.listen(8000, () => {
     console.log('Server is running on port 8000');
